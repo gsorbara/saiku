8beb2d8dc298318299ac8c6f14400c52eb52ebbc
sarawutp <Sarawut.Panchalong@fao.org>
2013-01-29 11:34:20 +0700

    modify wrong path and branch for saiku-ui

--- buildall.bat
+++ buildall.bat
@@ -10,12 +10,12 @@ cd ..
 call git submodule init
 call git submodule update
 echo "Building saiku-ui"
-cd ..\saiku-ui
-call git checkout master
+cd saiku-ui
+call git checkout release
 call git pull
 call mvn clean package install:install-file -Dfile=target/saiku-ui-2.4.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=2.4 -Dpackaging=war
 echo "Building saiku-server"
-cd ..\saiku\saiku-server
+cd ..\saiku-server
 call mvn clean package
 echo "Building saiku-bi-platform-plugin-util"
 cd ..\saiku-bi-platform-plugin-util
