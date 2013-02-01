041293fb374f762baf51c16b72292e7616f115ec
sarawutp <Sarawut.Panchalong@fao.org>
2013-02-01 17:20:48 +0700

    add sainity service check - remove unused import

--- saiku-core/saiku-web/src/main/java/org/saiku/web/rest/resources/SanityCheckResource.java
+++ saiku-core/saiku-web/src/main/java/org/saiku/web/rest/resources/SanityCheckResource.java
@@ -21,8 +21,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response.Status;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
 
 import org.saiku.olap.dto.SaikuConnection;
 import org.saiku.service.olap.OlapDiscoverService;
